ec22e787d8fe90bc7ef09c3efda34e354d401a5a
rezaulsajib <rksajib@gmail.com>
2016-08-20 23:03:12 +0600

    removed try/catch statement

--- src/main/java/com/mangobazar/controller/ProductController.java
+++ src/main/java/com/mangobazar/controller/ProductController.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,34 +37,20 @@ public class ProductController {
 
 	
 	@JsonView(ProductViews.View.class)
-	 @PreAuthorize("hasRole('ROLE_ADMIN')")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PostMapping
 	@ApiOperation(value = "create product", notes = "Creates and returns a product")
 	public void createProduct(@RequestBody @JsonView(ProductViews.Create.class) Product product,
 			HttpServletResponse response) {
-		try {
 			productService.createProduct(product);
-			response.setStatus(HttpStatus.CREATED.value());
-		} catch (Exception ex) {
-			response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-		}
 	}
 
 	@JsonView(ProductViews.View.class)
 	@ApiOperation(value = "Get Product", notes = "Returns all the product")
 	@GetMapping
 	public Collection<Product> Product(HttpServletResponse response) {
-		try {
-			Collection<Product> productList = productService.getAllProduct();
-			if (productList.isEmpty())
-				response.setStatus(HttpStatus.NO_CONTENT.value());
-			else
-				response.setStatus(HttpStatus.OK.value());
-			return productList;
-		} catch (Exception ex) {
-			response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-			return null;
-		}
+		return productService.getAllProduct();
+		
 
 	}
 
@@ -74,17 +59,7 @@ public class ProductController {
 	@GetMapping(value = "/{id}")
 	public Product findOne(@ApiParam(value = "id of the product", required = true) @PathVariable long id,
 			HttpServletResponse response) {
-		try {
-			Product product = productService.getProduct(id);
-			if (product != null)
-				response.setStatus(HttpStatus.OK.value());
-			else
-				response.setStatus(HttpStatus.NO_CONTENT.value());
-			return product;
-		} catch (Exception ex) {
-			response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-			return null;
-		}
+		return  productService.getProduct(id);
 
 	}
 
